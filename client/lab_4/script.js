9def083dde4c172a987c5a6ac93a2f7d8240cdd6
ali-b4 <chargersflt@gmail.com>
2021-09-26 19:56:02 -0400

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -4,7 +4,38 @@ const totalSlides = slides.length;
 
 document.
     querySelector('carousel_button_next')
-    .addEventListener
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
 
-/*Rather than using "document.getElementsByClassName" please use "document.querySelectorAll"
-Rather than using "document.getElementByID" please use "document.querySelector" */
+document.
+    querySelector('carousel_button_prev')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item_visible');
+        slide.classList.add('carousel_item_hidden');
+    }
+    slides[slidePosition].classList.add('carousel_item_visible');
+}
+
+function moveToNextSlide(){
+    if (slidePosition === totalSlides-1) {
+        slidePosition=0;
+    } else {
+        slidePosition ++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide(){
+    if (slidePosition === 0) {
+        slidePosition=totalSlides-1;
+    } else {
+        slidePosition --;
+    }
+    updateSlidePosition();
+}
