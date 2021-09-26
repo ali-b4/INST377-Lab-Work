89745ca2a109af0ca97f326d15f86760a2a7007b
ali-b4 <chargersflt@gmail.com>
2021-09-26 19:29:46 -0400

    asdfas

---
+++ client/lab_4/script.js
@@ -0,0 +1,10 @@
+let slidePosition = 0:
+const slides = document.querySelectorAll('carousel_item');
+const totalSlides = slides.length;
+
+document.
+    querySelector('carousel_button_next')
+    .addEventListener
+
+/*Rather than using "document.getElementsByClassName" please use "document.querySelectorAll"
+Rather than using "document.getElementByID" please use "document.querySelector" */
