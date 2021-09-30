7729b97197992c4aaf92ccb5668418397ecbcb9a
ali-b4 <chargersflt@gmail.com>
2021-09-29 23:25:40 -0400

    sdasda

---
+++ client/doodle/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGameOver = false
+    let platformCount=5
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace +'px'
+        doodler.style.bottom = doodlerBottomSpace +'px'
+
+    }
+
+    function createPlatforms() {
+        for (let i=0; i<platformCount; i++) {
+            let platformSpace = 600/platformCount
+            let newPlatBottom = 100 + i * platformSpace
+        }
+    }
+
+
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+
+        }
+    }
+
+    start()
+})
