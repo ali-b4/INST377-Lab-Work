77be47d1f19aad16dac359162fb04d58ad700d57
ali-b4 <chargersflt@gmail.com>
2021-10-06 20:49:07 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -68,4 +68,24 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-})
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      rotate()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
+    }
+  }
+  document.addEventListener('keyup', control)
+
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  
