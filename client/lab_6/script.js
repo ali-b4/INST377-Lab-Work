925602803ff8eb624d47fc45804048982d5eb15d
ali-b4 <chargersflt@gmail.com>
2021-10-14 02:25:34 -0400

    Merge branch 'main' of https://github.com/UMD-INST377/INST377-Lab-Work

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,28 +1,37 @@
-const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-
 const names = [];
 
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => names.push(...data))
-    
-function findMatches(wordToMatch, names) {
-  return names.filter(place => {
-    return names.filter(place => {
+async function fetchRequest(evt) {
+  const request = await fetch(url);
+  const json = await request.json();
+  console.table(json);
+  return json;
+}
+const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+const data = fetchRequest(url)
+//fetch(endpoint)
+//  .then((blob) => blob.json())
+//  .then((data) => names.push(...data));
 
-        const regex = new RegExp(wordToMatch, 'gi');
-            return place.names.match(regex) || place.state.match(regex)
-        })
-    })
+function findMatches(wordToMatch, data) {
+  return data.filter((place) => names.filter((place) => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.names.match(regex) || place.state.match(regex);
+  }));
 }
 
 function displayMatches() {
-    console.log(this.value);
-}
+  const matchArray = findMatches(this.value, names);
+  const html = matchArray.map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = play.city.replace(regex, '<span class = "h1">$ {this.value}</span');
+      const stateName = place.state.replace(regex, '<span class="h1"> ${this.value}</span>');
+      return 
+      '<li><span class = "name"> ${cityName}, ${stateName}</span></li>';
+  }
+}).join('');
 
 const searchInput = document.querySelector('.search');
-const suggest = document.querySelector('.suggestions');
-
+const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
