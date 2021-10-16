d16b07283e0ada9b4b2de87515d04e6da31d33c4
ali-b4 <chargersflt@gmail.com>
2021-10-16 19:13:02 -0400

    sregs

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,37 +1,20 @@
-const names = [];
-
 async function fetchRequest(evt) {
-  const request = await fetch(url);
-  const json = await request.json();
-  console.table(json);
-  return json;
-}
-const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
-const data = fetchRequest(url)
-//fetch(endpoint)
-//  .then((blob) => blob.json())
-//  .then((data) => names.push(...data));
-
-function findMatches(wordToMatch, data) {
-  return data.filter((place) => names.filter((place) => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.names.match(regex) || place.state.match(regex);
-  }));
-}
-
-function displayMatches() {
-  const matchArray = findMatches(this.value, names);
-  const html = matchArray.map((place) => {
-      const regex = new RegExp(this.value, 'gi');
-      const cityName = play.city.replace(regex, '<span class = "h1">$ {this.value}</span');
-      const stateName = place.state.replace(regex, '<span class="h1"> ${this.value}</span>');
-      return 
-      '<li><span class = "name"> ${cityName}, ${stateName}</span></li>';
+  try {
+    const request = await fetch(url);
+    const json = await request.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    return err;
   }
-}).join('');
+}
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+function mainThread() {
+  console.log('loaded main script');
+  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+  const inputBox = document.querySelector('#zipcode');
+  document.addEventListener('input', (event) => {
+    console.log(event.target.value);
+  });
+}
