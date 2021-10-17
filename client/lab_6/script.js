87170cf616bae05b8b17c0f7ad6bfea1ff65e818
ali-b4 <chargersflt@gmail.com>
2021-10-16 20:01:08 -0400

    adfasd

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,4 +1,4 @@
-async function fetchRequest(evt) {
+async function fetchRequest(url) {
   try {
     const request = await fetch(url);
     const json = await request.json();
@@ -9,12 +9,30 @@ async function fetchRequest(evt) {
   }
 }
 
-function mainThread() {
+async function mainThread() {
   console.log('loaded main script');
   const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
   const inputBox = document.querySelector('#zipcode');
+  const listOfFilteredItems = document.querySelector('.append-box');
+
+  const fetchElement = document.querySelector('.fetch');
+  const data = await fetchRequest(url);
+  const dataSet = [];
+
+  console.log('external dataset', data);
+
+
   document.addEventListener('input', (event) => {
     console.log(event.target.value);
+    const filteredList = data.filter((item, index) => {
+      const zipcode = event.target.value;
+      return item.zip === zipcode;
+    });
+    console.table(filteredList);
+    filteredList.forEach((item, index) => {
+      listOfFilteredItems.innerHTML += `<span class="resto-name">${item.name}</span> <br>`;
+    });
   });
 }
+
+window.onload = mainThread;
