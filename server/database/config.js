bfa718239dc550fb25de8ef7cb161767bf750279
ali-b4 <chargersflt@gmail.com>
2021-10-21 19:30:59 -0400

    Update config.js

--- server/database/config.js
+++ server/database/config.js
@@ -3,33 +3,21 @@ export default {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-<<<<<<< HEAD
-    host: '34.201.154.38',
-=======
     host: '34.207.233.215',
->>>>>>> 5d2c22c3bdbb7d4e9c62a093abb6845d7ee00f15
     dialect: 'mysql'
   },
   test: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-<<<<<<< HEAD
-    host: '34.201.154.38',
-=======
     host: '34.207.233.215',
->>>>>>> 5d2c22c3bdbb7d4e9c62a093abb6845d7ee00f15
     dialect: 'mysql'
   },
   production: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-<<<<<<< HEAD
-    host: '34.201.154.38',
-=======
     host: '34.207.233.215',
->>>>>>> 5d2c22c3bdbb7d4e9c62a093abb6845d7ee00f15
     dialect: 'mysql'
   }
 };
