f7eeaebc4fbad7bce4fd9c3be54acda89e88d108
ali-b4 <chargersflt@gmail.com>
2021-10-22 02:42:54 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -14,6 +14,17 @@ async function mainThread() {
   const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
   const inputBox = document.querySelector('#zipcode');
   const listOfFilteredItems = document.querySelector('.append-box');
+  const ACCESSTOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESSTOKEN}`, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+  }).addTo(mymap);
 
   const fetchElement = document.querySelector('.fetch');
   const data = await fetchRequest(url);
@@ -36,7 +47,7 @@ async function mainThread() {
 
 window.onload = mainThread;
 
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+/* const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -46,3 +57,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   zoomOffset: -1,
   accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+*/
