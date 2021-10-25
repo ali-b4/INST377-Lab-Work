1856c60601c0020f2a7e0c409134785900b5b7e9
ali-b4 <chargersflt@gmail.com>
2021-10-24 23:06:11 -0400

    Update script.js

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -10,6 +10,7 @@ async function fetchRequest(url) {
 }
 
 function filterFunction(event, data, list, mymap) {
+  list.innerHTML = '';
   console.log(event.target.value);
   const filteredList = data.filter((item, index) => {
     const zipcode = event.target.value;
@@ -17,12 +18,15 @@ function filterFunction(event, data, list, mymap) {
   });
   console.table(filteredList);
 
-  const limitedList = filteredList.slice(0,5);
+  const limitedList = filteredList.slice(0, 5);
 
   limitedList.forEach((item, index) => {
+    const point = item.geocoded_column_1;
+    const latLong = point.coordinates;
+    const marker = latLong.reverse();
     list.innerHTML += `<span class="resto-name">${item.name}</span> <br>`;
-    console.log(item.geocoded_column_1.coordinates);
-    L.marker([item.geocoded_column_1]).addTo(mymap);
+    console.log(marker);
+    L.marker([marker]).addTo(mymap);
   });
 }
 
@@ -49,18 +53,6 @@ async function mainThread() {
 
   console.log('external dataset', data);
 
-  document.addEventListener('input', (event) => { filterFunction(event, data, listOfFilteredItems); });
+  document.addEventListener('input', (event) => { filterFunction(event, data, listOfFilteredItems, mymap); });
 }
 window.onload = mainThread;
-
-/* const mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-*/
